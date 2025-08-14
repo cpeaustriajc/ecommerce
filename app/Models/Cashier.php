077ed7f9646de521c3876f7c6ba130571da67ab8
cpeaustriajc <cpe.austria.johncarlo@gmail.com>
2025-08-14 18:32:51 +0800

    chore: init

---
+++ app/Models/Cashier.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cashier extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\CashierFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
---
+++ resources/js/pages/storefront.tsx
@@ -0,0 +1,12 @@
+import { Head } from '@inertiajs/react';
+
+export default function StoreFront() {
+    return (
+        <>
+            <Head title="Storefront">
+                <link rel="preconnect" href="https://fonts.bunny.net" />
+                <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+            </Head>
+        </>
+    );
+}
